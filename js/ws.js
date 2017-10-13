5043c014d338146bb0e313499fd134430260a4f8
antonioMateos <antonio@freshcore.es>
2017-10-13 10:33:57 +0200

    Initial Commit

---
+++ js/ws.js
@@ -0,0 +1,102 @@
+var socket = io();
+
+socket.on("connect", function(msg){
+
+  console.log("socket ON",msg);
+
+});
+
+socket.on("response", function(newResp){
+
+  var data = newResp;
+  console.log(data);
+
+  /* Answer TEMPLATE */
+  var response = data;
+
+  $('#responseList').prepend(tweet);
+
+  //STATS
+  stats();
+
+});
+
+// START <> STOP Streaming
+// TO DO --> Add key press font intro on search!!!
+$('#start-btn').click(function(){
+
+	var search = $('#input-url').val(); // <-- Get input value
+	// TO DO --> Fix text for correct search
+
+	if(search!=""){
+
+		cleanRespList(); // <-- Clean responseList
+		$('main').addClass('searching'); // CHANGE STYLE TO STOP STREAMING
+		$('#search').show();
+		$('#search').text(search); // <-- Write tracked hashtag in title
+		socket.emit('start',search); // <-- Socket emit hashtag to server
+		responseMsg("start");
+
+	} else {
+
+		responseMsg("empty");
+
+	}
+
+});
+
+$('#stop-btn').click(function(){
+	$('#search').val("");
+	socket.emit('stop');
+	$('main').removeClass('searching'); // CHANGE STYLE TO START STREAMING
+	responseMsg("stop");
+});
+
+// STATS Fn
+var nt = 0; // Number of answers received
+function stats() {
+	$('.stats').show(); // show STATS
+	nt += 1;
+	$('.stats p span').text(nt);
+}
+
+// RESPONSE MSGs
+function responseMsg(response) {
+
+	// Toast Config
+	var resp;
+	var className = "response-msg";
+
+	var msg = {
+		empty: "Please, write a url!!!",
+		stop: "Receiving stopped!",
+		start: "Call started!"
+	};
+
+	if(response=="empty") {
+		resp = msg.empty;
+		className += " error";
+	}
+
+	if(response=="stop") {
+		resp = msg.stop;
+		className += " stop";
+	}
+
+	if(response=="start") {
+		resp = msg.start;
+		className += " success";
+	}
+
+	// Materialize.toast(message, displayLength, className, completeCallback);
+	var toastContent = "<span>"+resp+"</span>";
+	var time = 4000;
+	Materialize.toast(toastContent, time, className); // 4000 is the duration of the toast
+
+}
+
+// REFRESH Tw List
+function cleanRespList() {
+	//Refresh ul tweetList
+	$('#responseList').html("");
+};
---
+++ server.js
@@ -0,0 +1,75 @@
+//'use strict';
+var express = require('express'),
+	app     = express(),
+	path    = require('path'),
+	http	= require('http').Server(app),
+  io = require("socket.io")(http), // app or http
+	bodyParser = require('body-parser');
+
+console.log('- - - - Iniciando entorno');
+// CORS --> Cabeceras correctas
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+// Parsers --> Poder mandar y recibir JSON
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+console.log('- - - - Middlewares cargados...');
+
+console.log('- - - - FRONT ROUTES');
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function (request, response, next) {
+	response.sendFile(path.join(__dirname+'/index.html'));
+});
+
+console.log('- - - - END FRONT ROUTES');
+
+//CALL  CONFIG
+config = require('./config') // <-- CONFIG file
+// <-- TO DO > Environment variables
+
+//SOCKET IO
+console.log('- - - - SOCKET IO');
+
+io.on('connection', function(socket){
+
+  //START CONNECTION
+  var msg = 'Server ON';
+  socket.emit('connect',msg);
+
+  //URL SEARCH from front
+  socket.on('start',function(data){
+
+    var url = data;
+    //console.log(url);
+
+  });
+
+  //STOP STREAMING
+  socket.on('stop',function(){
+    console.log("- - - - STOP Streaming - - - -");
+  });
+  
+  // STANDARD SOCKETS
+  /*
+  socket.on('new-message', function(data) {
+    //messages.push(data);
+    console.log(data.text);
+    io.sockets.emit('messages', messages);
+  });
+  */
+
+});
+//END SOCKET IO
+
+// STARTING SERVER
+http.listen(process.env.PORT || 3000, function () {
+  console.log('- START SERVER - - - - - -\n');
+  console.log('Server Listening on http://localhost:' + (process.env.PORT || 3000))
+});
